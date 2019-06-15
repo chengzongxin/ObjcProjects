23630012f07427667cd3214024aa215bdd7dd137
Joe <8876142@qq.com>
2019-06-16 00:25:40 +0800

    Update

--- ObjcProjects/Message/MessageViewController.m
+++ ObjcProjects/Message/MessageViewController.m
@@ -7,28 +7,83 @@
 //
 
 #import "MessageViewController.h"
+#import <ReactiveObjC.h>
 
-@interface MessageViewController ()
+@interface MessageViewController ()<UITableViewDataSource,UITableViewDelegate>
+{
+    NSMutableArray *_datas;
+}
+@property (strong,nonatomic) UITableView *tableView;
 
 @end
 
 @implementation MessageViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    _datas = [NSMutableArray arrayWithObjects:@"123",@"123",@"123",@"123",@"123",@"123",@"123", nil];
     
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"back" style:UIBarButtonItemStylePlain target:nil action:nil];
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStylePlain];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self.view addSubview:self.tableView];
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:NSStringFromClass([UITableViewCell class])];
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"add" style:UIBarButtonItemStylePlain target:self action:@selector(_add)];
+    
+    UIView *header = [[UIView alloc] initWithFrame:CGRectMake(0, -88, self.view.frame.size.width, 88)];
+    header.backgroundColor = UIColor.orangeColor;
+    [self.tableView addSubview:header];
+    
+    
+    UIView *footer = [[UIView alloc] init];
+    footer.backgroundColor = UIColor.greenColor;
+    footer.layer.zPosition = -1;
+    [self.tableView addSubview:footer];
+    
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(0, 20, self.view.frame.size.width, 20);
+    label.text = @"----已经到底啦----";
+    label.textAlignment = NSTextAlignmentCenter;
+    [footer addSubview:label];
+    
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 88, 0);
+    [RACObserve(self.tableView, contentSize) subscribeNext:^(id  _Nullable x) {
+        CGSize size = [x CGSizeValue];
+        footer.frame = CGRectMake(0, size.height, self.view.frame.size.width, 88);
+    }];
+    
+    
+    //    RACSignal *button1Singnal = [[self.button1 rac_signalForControlEvents:UIControlEventTouchUpInside] map:^id _Nullable(__kindof UIControl * _Nullable value) {
+    //        NSLog(@"button1 = %@",value);
+    //        return [NSDate date];
+    //    }];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+    NSLog(@"%s",__FUNCTION__);
 }
-*/
+
+- (void)_add{
+    [_datas addObject:@"123"];
+    [self.tableView reloadData];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _datas.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class])];
+    cell.textLabel.text = _datas[indexPath.row];
+    return cell;
+}
 
 @end
